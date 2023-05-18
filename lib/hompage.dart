3e8b59574d8b0e4906df05f333e9bc1353f34cc6
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-18 13:57:01 +0100

    ...

--- lib/hompage.dart
+++ lib/hompage.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,7 +11,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-   late List currencies;
+  late final List currencies;
+  final List<MaterialColor> _colors = [Colors.blue, Colors.indigo, Colors.red];
 
   @override
   Future<void> initState() async {
@@ -20,11 +20,10 @@ class _HomePageState extends State<HomePage> {
     currencies = await getCurrencies();
   }
 
-  Future <List> getCurrencies() async{
-    String cryptoUrl="https://api.coinmarketcap.com/v1/ticker/?limit=50";
+  Future<List> getCurrencies() async {
+    String cryptoUrl = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
     http.Response response = await http.get(cryptoUrl as Uri);
     return json.decode(response.body);
-    
   }
 
   @override
@@ -36,13 +35,20 @@ class _HomePageState extends State<HomePage> {
       body: _cryptoWidget(),
     );
   }
-}
 
-Widget _cryptoWidget() {
-  return Container(
-    child: Flexible(
-      child: ListView.builder(
-          itemCount: 0, itemBuilder: (BuildContext context, int index) {}),
-    ),
-  );
+  Widget _cryptoWidget() {
+    return Container(
+      child: Flexible(
+        child: ListView.builder(
+          itemCount: currencies.length,
+          itemBuilder: (BuildContext context, int index) {
+            final Map currency = currencies[index];
+            final MaterialColor color = _colors[index % _colors.length];
+
+            return _getListItemUI(currency, color);
+          },
+        ),
+      ),
+    );
+  }
 }
